0f563b7529b3481d8f0a16a8db3f2e1b32d317c4
cwurexit <cwur@mailbox.org>
2020-09-17 10:46:04 +0200

    HTML set up

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,22 @@
-<h1>Testing...</h1>
+<h1>Hand Washing App</h1>
+
+<Timer />
+<HowTo />
+
+<h3>
+<a href="https://www.who.int/gpsc/clean_hands_protection/en/">Picture source</a>
+-
+<a href="https://freesound.org/people/metrostock99/sounds/345086/">Sound source</a>
+</h3>
 
 <script>
+import HowTo from './HowTo.svelte';
+import Timer from './Timer.svelte';
 
 </script>
 
 <style>
-
+    h1, h3 {
+        text-align: center;
+    }
 </style>
---
+++ src/Timer.svelte
@@ -0,0 +1,21 @@
+<div bp="grid">
+    <h2 bp="offset-5@md 4@md 12@sm">
+        Seconds Left:
+    </h2>
+</div>
+
+<ProgressBar />
+
+<button class="start">Start</button>
+
+<script>
+    import ProgressBar from './ProgressBar.svelte';
+</script>
+
+<style>
+    .start {
+        background-color: rgb(124, 28, 28);
+        width: 100%;
+        margin: 10px 0;
+    }
+</style>
